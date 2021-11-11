e5d935686b8c4b1c5dfba88faa87cd8d256f04da
felixdave <david@neatify.io>
2021-11-11 14:51:01 -0500

    clean requirements

--- commerce/settings.py
+++ commerce/settings.py
@@ -39,7 +39,6 @@ ALLOWED_HOSTS = [
 INSTALLED_APPS = [
     'auctions',
     'django_sass',
-    'livereload',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -56,9 +55,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
-
-
 ]
 
 ROOT_URLCONF = 'commerce.urls'
