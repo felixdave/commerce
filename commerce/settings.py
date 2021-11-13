ea8a11af9440ee015bfd2b1179b3fe567226d70d
felixdave <david@neatify.io>
2021-11-12 23:28:04 -0500

    loaded db

--- commerce/settings.py
+++ commerce/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'commerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'commercedb',
+        'USER':'postgres',
+        'PASSWORD':'SMLXL22',
+        'HOST':'LOCALHOST',
+        'PORT':'5432'
     }
 }
 
